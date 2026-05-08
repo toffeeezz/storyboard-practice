010c2889ff295ffb2dac98a83205278b1eeb055c
ttoaffers <animeone56@gmail.com>
2026-05-08 11:14:15 +0800

    updated imports

--- src/main/java/com/storyboard/graphx/node/comp/ArrowLine.java
+++ src/main/java/com/storyboard/graphx/node/comp/ArrowLine.java
@@ -1,6 +1,6 @@
 package com.storyboard.graphx.node.comp;
 
-import com.storyboard.graphx.Editor;
+import com.storyboard.graphx.ui.editor.Editor;
 import com.storyboard.utils.Vector2;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.shape.Line;
