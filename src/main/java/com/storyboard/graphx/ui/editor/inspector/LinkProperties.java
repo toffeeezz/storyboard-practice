ffe1955d3731557d50ca11ed8c4a1a4c52315e89
ttoaffers <animeone56@gmail.com>
2026-05-13 17:02:13 +0800

    transition combo box now properly updates to the names of the variables

--- src/main/java/com/storyboard/graphx/ui/editor/inspector/LinkProperties.java
+++ src/main/java/com/storyboard/graphx/ui/editor/inspector/LinkProperties.java
@@ -2,9 +2,14 @@ package com.storyboard.graphx.ui.editor.inspector;
 
 import com.storyboard.graphx.node.StoryNode;
 import com.storyboard.graphx.node.comp.ArrowLine;
+import com.storyboard.logic.GlobalVariable;
+import com.storyboard.logic.ProjectSettings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
@@ -13,6 +18,8 @@ public class LinkProperties extends VBox {
 
     @FXML private Label fromLabel;
     @FXML private Label toLabel;
+
+    @FXML private ComboBox<GlobalVariable> globalVarBox;
 
 
     LinkProperties(){
@@ -32,5 +39,24 @@ public class LinkProperties extends VBox {
         System.out.println("Display link");
         fromLabel.setText(line.getNodeOrigin().getId());
         toLabel.setText(line.getNodeTo().getId());
+        globalVarBox.setItems(ProjectSettings.getInstance().getGlobalVarList());
+
+        globalVarBox.setCellFactory(_ -> typeCell());
+        globalVarBox.setButtonCell(typeCell());
+    }
+
+    private ListCell<GlobalVariable> typeCell(){
+        return new ListCell<>() {
+            @Override
+            protected void updateItem(GlobalVariable globalVariable, boolean b) {
+                super.updateItem(globalVariable, b);
+                if (b || globalVariable == null) {
+                    textProperty().unbind();
+                    setText(null);
+                } else {
+                    textProperty().bind(globalVariable.nameProperty());
+                }
+            }
+        };
     }
 }
