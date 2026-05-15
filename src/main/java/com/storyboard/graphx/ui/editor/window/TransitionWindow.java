2de70b9abb05eaf6a394dd0b1129dd3238e8e99c
ttoaffers <animeone56@gmail.com>
2026-05-15 18:16:48 +0800

    now safely checks for any empty fields before saving the transition and closing

--- src/main/java/com/storyboard/graphx/ui/editor/window/TransitionWindow.java
+++ src/main/java/com/storyboard/graphx/ui/editor/window/TransitionWindow.java
@@ -1,8 +1,13 @@
 package com.storyboard.graphx.ui.editor.window;
 
+import com.storyboard.graphx.node.comp.ArrowLine;
+import com.storyboard.graphx.ui.editor.inspector.TransitionEntry;
+import com.storyboard.logic.Condition;
+import com.storyboard.utils.Alert;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -12,15 +17,22 @@ import javafx.stage.StageStyle;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TransitionWindow extends VBox {
 
     @FXML private FontIcon closeButton;
+    @FXML private Button addButton;
+
+    @FXML private VBox entryPane;
 
     private Stage stage;
 
-    public TransitionWindow(){
+    private final ArrowLine arrowLine;
+
+    public TransitionWindow(ArrowLine arrowLine){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/ui/editor/window/TransitionWindow.fxml"));
 
         loader.setRoot(this);
@@ -33,6 +45,7 @@ public class TransitionWindow extends VBox {
         }
 
         final double[] dragDelta = new double[2];
+        this.arrowLine = arrowLine;
 
         setOnMousePressed(e -> {
             dragDelta[0] = stage.getX() - e.getScreenX();
@@ -42,6 +55,12 @@ public class TransitionWindow extends VBox {
         setOnMouseDragged(e -> {
             stage.setX(e.getScreenX() + dragDelta[0]);
             stage.setY(e.getScreenY() + dragDelta[1]);
+        });
+
+        addButton.setOnAction(e -> {
+            arrowLine.getTransition().getConditionList().add(new Condition());
+            showTransitions();
+            e.consume();
         });
     }
 
@@ -58,11 +77,41 @@ public class TransitionWindow extends VBox {
         stage.setScene(scene);
 
         closeButton.setOnMousePressed(e -> {
+            List<String> errors = entryPane.getChildren().stream()
+                    .filter(n -> n instanceof TransitionEntry)
+                    .map(n -> (TransitionEntry) n)
+                    .map(TransitionEntry::validate)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+
+            if (!errors.isEmpty()) {
+                Alert.showWarning(String.join("\n", errors));
+                return;
+            }
+
+            entryPane.getChildren().stream()
+                    .filter(n -> n instanceof TransitionEntry)
+                    .map(n -> (TransitionEntry) n)
+                    .forEach(TransitionEntry::save);
+
             stage.close();
             e.consume();
         });
 
+        showTransitions();
         stage.showAndWait();
 
+    }
+
+    private void showTransitions(){
+        entryPane.getChildren().clear();
+
+        if (arrowLine.getTransition().getConditionList().isEmpty()) return;
+
+        List<Condition> conditionList = arrowLine.getTransition().getConditionList();
+
+        for(Condition condition : conditionList){
+            entryPane.getChildren().add(new TransitionEntry(entryPane, conditionList, condition));
+        }
     }
 }
