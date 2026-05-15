3c2887122ae3bb6644335047bfb13452b832f802
ttoaffers <animeone56@gmail.com>
2026-05-15 18:14:08 +0800

    -added a logic mode when more than two conditions (ongoing still) -added variables for the two nodes of the transitions

--- src/main/java/com/storyboard/logic/Transition.java
+++ src/main/java/com/storyboard/logic/Transition.java
@@ -1,5 +1,7 @@
 package com.storyboard.logic;
 
+import com.storyboard.graphx.node.StoryNode;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +11,39 @@ public class Transition {
         return conditionList;
     }
 
+    public enum LogicMode{
+        OR, AND
+    }
+
+    public LogicMode getLogicMode() {
+        return logicMode;
+    }
+
+    public void setLogicMode(LogicMode logicMode) {
+        this.logicMode = logicMode;
+    }
+
+    private LogicMode logicMode;
+
     private final List<Condition> conditionList = new ArrayList<>();
+
+    public StoryNode getToNode() {
+        return toNode;
+    }
+
+    public void setToNode(StoryNode toNode) {
+        this.toNode = toNode;
+    }
+
+    public StoryNode getFromNode() {
+        return fromNode;
+    }
+
+    public void setFromNode(StoryNode fromNode) {
+        this.fromNode = fromNode;
+    }
+
+    private StoryNode fromNode;
+    private StoryNode toNode;
 
 }
