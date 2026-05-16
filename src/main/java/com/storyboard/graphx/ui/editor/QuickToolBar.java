33b5dd9c24c02345d078bb8a8c58c0f73857cc6e
ttoaffers <animeone56@gmail.com>
2026-05-16 20:06:46 +0800

    now creates button automatically for each subclasses of StoryNode

--- src/main/java/com/storyboard/graphx/ui/editor/QuickToolBar.java
+++ src/main/java/com/storyboard/graphx/ui/editor/QuickToolBar.java
@@ -1,19 +1,24 @@
 package com.storyboard.graphx.ui.editor;
 
+import com.storyboard.graphx.node.StoryNode;
+import com.storyboard.utils.Vector2;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import org.reflections.Reflections;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
 
 public class QuickToolBar extends VBox {
 
-    @FXML private Button addDialogueButton;
 
-    public QuickToolBar(){
+
+    public QuickToolBar(Editor editor){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/storyboard/graphx/ui/editor/QuickToolBar.fxml"));
 
         loader.setRoot(this);
@@ -25,10 +30,29 @@ public class QuickToolBar extends VBox {
             throw new RuntimeException(e);
         }
 
+        Reflections reflections = new Reflections("com.storyboard.graphx.node");
+        Set<Class<? extends StoryNode>> nodeSubClasses = reflections.getSubTypesOf(StoryNode.class);
+
+        for(Class< ? extends StoryNode> storyNode : nodeSubClasses){
+            Button button = createButton(editor, storyNode);
+            getChildren().add(button);
+        }
 
     }
 
-    protected void onDialogueButtonPressed(EventHandler<MouseEvent> e){
-        addDialogueButton.setOnMousePressed(e);
+    private static Button createButton(Editor editor, Class<? extends StoryNode> storyNode) {
+        Button button = new Button(storyNode.getSimpleName());
+        button.setPrefWidth(Double.MAX_VALUE);
+        button.setOnMousePressed(e -> {
+            try{
+                StoryNode node = storyNode.getDeclaredConstructor(Editor.class).newInstance(editor);
+                Vector2 pos = editor.camera.getMouseWorldPos(e);
+                editor.addNode(node, pos);
+            } catch (InvocationTargetException | InstantiationException | NoSuchMethodException |
+                     IllegalAccessException exception) {
+                throw new RuntimeException(exception);
+            }
+        });
+        return button;
     }
 }
